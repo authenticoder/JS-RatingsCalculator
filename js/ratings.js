6baa60f7203e3a91cb3898e17c07517bfc69ff48
authenticoder <authenticoderio@gmail.com>
2021-11-16 18:09:54 +0300

    solve challenge 10 to 15

--- js/ratings.js
+++ js/ratings.js
@@ -15,4 +15,16 @@ function collect_ratings() {
         ratings.count += parseInt(element.value);
         ratings.sum += parseInt(element.value) * rating;
     })
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings;
 }
+
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+    
+})
